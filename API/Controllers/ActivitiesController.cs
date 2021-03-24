c5c79d16de8bb77a9186f76244acf0626e3eeef2
fazaltech <38839909+fazaltech@users.noreply.github.com>
2021-03-24 18:42:31 +0500

    Adding a Delete handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
@@ -34,5 +35,26 @@ namespace API.Controllers
 
         }
 
+        [HttpPost]
+
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+   
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit (Guid id,Edit.Command command)
+        {
+            command.Id=id;
+            return await _mediator.Send(command);
+
+        }
+         [HttpDelete("{id}")]
+
+         public async Task<ActionResult<Unit>> Delete(Guid id)
+         {
+               return await _mediator.Send(new Delete.Command{Id=id}); 
+         }
     }
 }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -15,14 +17,17 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
+           
             public Handler(DataContext context)
             {
+               
                 _context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities =await _context.Activities.ToListAsync();
+               
+                var activities = await _context.Activities.ToListAsync();
 
                 return activities;
             }
